0e931a754a3d12981aaf44dfc5db8445a21633f3
SeongHo Bang <shbang37@gmail.com>
2026-02-09 18:11:54 +0900

    Initial Commit

---
+++ ZAKAR/ZAKARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZAKARApp.swift
+//  ZAKAR
+//
+//  Created by SeongHo Bang on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ZAKARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
